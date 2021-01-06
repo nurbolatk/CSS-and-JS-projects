6b6bdd1d2a44ea3079662bbc5917e6db5e20a279
nurbolatk <nurbolattk@gmail.com>
2021-01-06 23:00:09 +0600

    Style project list and generate projects from folders

--- populateProjects.js
+++ populateProjects.js
@@ -1,21 +1,45 @@
-const rootFolder = './';
-const fs = require('fs');
+const fs = require('fs')
+const rootFolder = './'
 
-let body = ''
-
+// let body = document.querySelector('body')
+const template = `
+  <div class="card">
+    <img src="./23-voiceinator/23.png" alt="Voicenator" class="project-img" />
+    <div class="card-body">
+      <a href="./23-voiceinator/index.html">Project 23 - Voicenator</a>
+    </div>
+  </div>
+`
 function createProjectCard(name) {
   return `
-    <a href="./${name}/index.html">${name}</a>
-  `
+  <div class="card">
+    <img src="./23-voiceinator/23.png" alt="Voicenator" class="project-img" />
+    <div class="card-body">
+      <a href="./${name}/index.html">Project 23 - name</a>
+    </div>
+  </div>
+`
 }
+let projectsHTML = ''
 
-const files = fs.readdirSync(rootFolder);
+const files = fs.readdirSync(rootFolder)
 files.forEach(file => {
   const stats = fs.statSync(file)
-  if(stats.isDirectory() && /^\d+/.test(file)) {
-    body += (createProjectCard(file))
+  if (stats.isDirectory() && /^\d+/.test(file)) {
+    projectsHTML += createProjectCard(file)
   }
-});
+})
 
-console.log(body)
-
+try {
+  const data = fs.readFileSync('index.html', 'utf8')
+  fs.writeFileSync(
+    'index.html',
+    data.replace(
+      /(<div class="project-list">)[\s\S]+(<\/div>)/,
+      `<div class="project-list">${projectsHTML}<\/div>`
+    )
+  )
+  //file written successfully
+} catch (err) {
+  console.error(err)
+}
